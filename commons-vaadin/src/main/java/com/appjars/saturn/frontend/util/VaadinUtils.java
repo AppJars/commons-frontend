9332e7a0515296e970de12fc1587f36adb4d9c1f
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-09-14 10:30:46 -0300

    feat: add UI event bus
    Close #6

--- commons-vaadin/src/main/java/com/appjars/saturn/frontend/util/VaadinUtils.java
+++ commons-vaadin/src/main/java/com/appjars/saturn/frontend/util/VaadinUtils.java
@@ -1,6 +1,12 @@
 package com.appjars.saturn.frontend.util;
 
 import java.util.function.Consumer;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.shared.Registration;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -15,4 +21,21 @@ public class VaadinUtils {
     return t;
   }
 
+  /**Register a UI event bus that is active while the component is attached.
+   * The event bus will unregister itself when the component is detached.
+   * @see #fireUIEvent(ComponentEvent)*/
+  public static <T extends ComponentEvent<?>> void addUIListener(Component component, Class<T> eventType,
+      ComponentEventListener<T> listener) {
+    component.addAttachListener(ev -> {
+      Registration registration = ComponentUtil.addListener(ev.getUI(), eventType, listener);
+      component.addDetachListener(event -> registration.remove());
+    });
   }
+
+  /**Trigger an event in the {@linkplain #addUIListener(Component, Class, ComponentEventListener) UI event bus}.**/
+  public static void fireUIEvent(ComponentEvent<?> event) {
+    UI ui = event.getSource().getUI().get();
+    ComponentUtil.fireEvent(ui, event);
+  }
+
+}
