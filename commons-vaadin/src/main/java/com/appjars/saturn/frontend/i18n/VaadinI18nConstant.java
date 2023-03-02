1abe624bce233a2f1bc6bcd3b988f3e6dcfc9304
JuaniVega <juanivega0206@gmail.com>
2023-03-02 13:00:20 -0300

    feat: adds constants to commons keys

---
+++ commons-vaadin/src/main/java/com/appjars/saturn/frontend/i18n/VaadinI18nConstant.java
@@ -0,0 +1,23 @@
+package com.appjars.saturn.frontend.i18n;
+
+public class VaadinI18nConstant {
+
+  private VaadinI18nConstant() {}
+
+  public static final String FILTER_MESSAGE_KEY = "appjars.commonsfrontend.filter";
+  public static final String CLEAR_MESSAGE_KEY = "appjars.commonsfrontend.clear";
+  public static final String ACTIONS_MESSAGE_KEY = "appjars.commonsfrontend.actions";
+  public static final String SAVE_MESSAGE_KEY = "appjars.commonsfrontend.save";
+  public static final String CANCEL_MESSAGE_KEY = "appjars.commonsfrontend.cancel";
+  public static final String CLOSE_MESSAGE_KEY = "appjars.commonsfrontend.close";
+  public static final String EDIT_MESSAGE_KEY = "appjars.commonsfrontend.edit";
+  public static final String DELETE_MESSAGE_KEY = "appjars.commonsfrontend.delete";
+  public static final String BACK_MESSAGE_KEY = "appjars.commonsfrontend.back";
+  public static final String CONFIRM_MESSAGE_KEY = "appjars.commonsfrontend.confirm";
+  public static final String ERROR_MESSAGE_KEY = "appjars.commonsfrontend.error";
+  public static final String LANGUAGE_MESSAGE_KEY = "appjars.commonsfrontend.language";
+  public static final String TRANSLATION_MESSAGE_KEY = "appjars.commonsfrontend.translation";
+  public static final String WARNING_MESSAGE_KEY = "appjars.commonsfrontend.warning";
+  public static final String DESCRIPTION_MESSAGE_KEY = "appjars.commonsfrontend.description";
+
+}
