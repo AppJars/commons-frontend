24db4d38da4ce805dac79ee4c6fac6aac16694af
Nicolás Luis González Paz <ngonzalez@flowingcode.com>
2023-03-21 12:39:31 -0300

    remove!: remove commons-frontend-security-keycloak module
    BREAKING CHANGE: commons frontend will no longer support a Keycloak implementation.

--- commons-frontend-security-keycloak/src/main/java/com/appjars/saturn/frontend/spring/SpringSecurityModule.java
+++
@@ -1,9 +0,0 @@
-package com.appjars.saturn.frontend.spring;
-
-/**
- * Empty marker class used to identify this package when using type-safe basePackageClasses in
- * Spring @ComponentScan.
- */
-public class SpringSecurityModule {
-    
-}
